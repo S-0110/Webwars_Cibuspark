90ebec74c7d3d6b54ed9294e59b2a0d86fdc060c
Harsh Sharma <hbharadwaj627@gmail.com>
2025-02-02 18:26:07 +0530

    Logo page Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Threed from './hreed.jsx'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Threed/>     
+    </div>
   )
 }
 
---
+++ src/hreed.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+import Spline from '@splinetool/react-spline';
+
+export default function hreed() {
+  return (
+    <div className='logo'>
+        <Spline scene="https://prod.spline.design/kW4D3creaqVLNFz8/scene.splinecode" />
+    </div>
+    );
+}
+
